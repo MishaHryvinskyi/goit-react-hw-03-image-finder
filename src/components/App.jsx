1ec0fee2eea3a24accde0185419ee42a9e3694f8
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-06-06 23:58:05 +0300

    FETCH

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,60 @@
-import { Component } from "react";
-import { Searchbar } from "./Searchbar/Searchbar";
-import { ToastContainer } from "react-toastify";
+// App.jsx
+import React, { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
 
- class  App extends Component {
-state= {
-  images: [],
-  loading: false,
-  search: '',
-};
+class App extends Component {
+  state = {
+    images: [],
+    loading: false,
+    search: '',
+  };
 
-handleFormSubmit = search => {
-  this.setState({ search });
-}
+  handleFormSubmit = search => {
+    this.setState({ search });
+  };
 
   componentDidMount() {
-    this.setState({ loading: true });
     
-    setTimeout(() => {
-      fetch("https://pixabay.com/api/?q=cat&page=1&key=35643945-433c06e40cd86730ec72beccd&image_type=photo&orientation=horizontal&per_page=12")
-    .then((res) => res.json())
-      .then((data) => {
-        this.setState({ images: data.hits, loading: false });
-      })
-      .finally(this.setState({ loading: false }))
-      .catch((error) => {
-        console.log("Error fetching data:", error);
-      })
-    }, 1000)
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.search !== this.state.search) {
+      console.log('Змінилося');
+      this.setState({ loading: true });
+      
+      fetch(
+        `https://pixabay.com/api/?q=${this.state.search}&page=1&key=35643945-433c06e40cd86730ec72beccd&image_type=photo&orientation=horizontal&per_page=12`
+      )
+        .then(res => res.json())
+        .then(data => {
+          this.setState({ images: data.hits, loading: false });
+        })
+        .catch(error => {
+          console.log('Error fetching data:', error);
+        });
+    }
   }
 
   render() {
     const { images, loading } = this.state;
-    
+
     return (
       <div>
-        <Searchbar onSubmit={this.handleFormSubmit}/>
-       {images.length > 0 && (
-          <ul className="gallery">
-            {images.map((image) => (
-              <li key={image.id} className="gallery-item">
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        {images.length > 0 && (
+          <ul>
+            {images.map(image => (
+              <li key={image.id}>
                 <img src={image.webformatURL} alt={image.tags} />
               </li>
             ))}
           </ul>
         )}
         {loading && <h1>Загружаємо...</h1>}
-        <ToastContainer
-            position="top-left"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="dark"
-        />
       </div>
     );
   }
-};
-
+}
 
 export default App;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,61 +1,46 @@
 import React, { Component } from 'react';
-import { toast } from 'react-toastify';
-import { 
-    Search,
-    SearchForm,
-    SearchFormBtn,
-    SearchInput
- } from './Searchbar.styled';
- import { FcSearch } from 'react-icons/fc';
-
+import { Search, SearchForm, SearchFormBtn, SearchInput } from './Searchbar.styled';
+import { FcSearch } from 'react-icons/fc';
 
 export class Searchbar extends Component {
-    state = {
-        search: '',
+  state = {
+    search: '',
+  };
+
+  handleSearchChange = e => {
+    this.setState({ search: e.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    if (this.state.search.trim() === '') {
+      return alert("Нічого не ввели");
     }
 
-handleSearchChange = e => {
-    this.setState({ search: e.currentTarget.value.toLowerCase() });
-}
-handleSubmit = e => {
-    e.preventDefault();
-    
-    if(this.state.search.trim() === '') {
-        return toast.error('🦄 Wow so easy!', {
-            position: "top-left",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-            });
-    }
     this.props.onSubmit(this.state.search);
     this.setState({ search: '' });
-}
+  };
 
-render() {
+  render() {
     const { search } = this.state;
     return (
-        <Search>
-            <SearchForm onSubmit={this.handleSubmit}>
-                <SearchFormBtn type="submit">
-                <FcSearch size="24"/>
-                </SearchFormBtn>
+      <Search>
+        <SearchForm onSubmit={this.handleSubmit}>
+          <SearchFormBtn type="submit">
+            <FcSearch size="24" />
+          </SearchFormBtn>
 
-                <SearchInput
-                type="text"
-                autoComplete="off"
-                autoFocus
-                placeholder="Search images and photos"
-                value={search}
-                onChange={this.handleSearchChange}
-                />
-            </SearchForm>
-        </Search>
-        )
-    }
+          <SearchInput
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={search}
+            onChange={this.handleSearchChange}
+          />
+        </SearchForm>
+      </Search>
+    );
+  }
 }
-
