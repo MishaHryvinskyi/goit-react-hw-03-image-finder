baa8393025177a7a83f139b3a323e6d104fe2708
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-06-07 12:09:27 +0300

    ч

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,15 @@
+// import React, { Component } from "react";
+
+// class ImageGallery extends Component {
+//     render() {
+//         return(
+//             <ul>
+//             {images.map(image => (
+//               <li key={image.id}>
+//                 <img src={image.webformatURL} alt={image.tags} />
+//               </li>
+//             ))}
+//           </ul>
+//         )
+//     }
+// }
